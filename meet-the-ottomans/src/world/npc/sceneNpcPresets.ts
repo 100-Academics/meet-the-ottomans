4f3c81ea3ac0daf7d10f8ac85f41acadf68118a8
Kelvin Zimmerman <71656069+afterglow79@users.noreply.github.com>
2026-05-18 12:18:34 +0000

    forgot to add files, as always

---
+++ meet-the-ottomans/src/world/npc/sceneNpcPresets.ts
@@ -0,0 +1,35 @@
+import { Vec3 } from "playcanvas";
+import type { NpcSceneSpawnOptions, NpcSpawnPoint } from "./sceneNpcSystem";
+
+export const DEFAULT_BATTLE_NPC_SPAWN_OPTIONS: NpcSceneSpawnOptions = {
+    modelPath: "test/armored_king.glb",
+    modelRotation: new Vec3(-90, 0, 0),
+    modelScale: new Vec3(2, 2, 2),
+    modelHeightOffset: 2,
+    facingYawOffsetDegrees: 180,
+    hitboxRadius: 1.2
+};
+
+export const LEGNICA_NPC_SPAWN_POINTS: NpcSpawnPoint[] = [
+    { id: 1, team: "foe", x: 6, z: 0 },
+    { id: 2, team: "friend", x: -5, z: 0 },
+    { id: 3, team: "foe", x: 5, z: 0 },
+    { id: 4, team: "friend", x: -7, z: 0 },
+    { id: 5, team: "foe", x: 7, z: 0 },
+    { id: 6, team: "friend", x: -4, z: 0 },
+    { id: 7, team: "foe", x: 4, z: 0 },
+    { id: 8, team: "friend", x: -6, z: 0 },
+    { id: 9, team: "foe", x: 8, z: 0 },
+    { id: 10, team: "friend", x: -8, z: 0 },
+    { id: 11, team: "foe", x: 9, z: 0 },
+    { id: 12, team: "friend", x: -9, z: 0 }
+];
+
+export const AIN_JALUT_NPC_SPAWN_POINTS: NpcSpawnPoint[] = [
+    { id: 1, team: "foe", x: 8, z: -2 },
+    { id: 2, team: "friend", x: -8, z: 2 },
+    { id: 3, team: "foe", x: 10, z: 2 },
+    { id: 4, team: "friend", x: -10, z: -2 },
+    { id: 5, team: "foe", x: 6, z: 4 },
+    { id: 6, team: "friend", x: -6, z: -4 }
+];
