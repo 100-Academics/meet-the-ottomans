4a337aefc5c8102f7c64b1a4bc250a29e51a8332
Kelvin Zimmerman <71656069+afterglow79@users.noreply.github.com>
2026-05-21 12:22:27 +0000

    change bow draw speed

--- meet-the-ottomans/src/player/player.ts
+++ meet-the-ottomans/src/player/player.ts
@@ -18,7 +18,7 @@ export class Player{
     private team = 'friend'; // Player is always on the 'friend' team
     private readonly swordWeapon = new Sword(4, 25);
     private readonly gunWeapon = new Gun(100, 100, 12);
-    private readonly bowWeapon = new Bow(50, 100, 20);
+    private readonly bowWeapon = new Bow(50, 100, 20, 250);
     private equippedWeapon: Weapon = this.swordWeapon;
 
     constructor(app: AppBase, initialPosition: Vec3 = new Vec3(0, 8, 8)) {
