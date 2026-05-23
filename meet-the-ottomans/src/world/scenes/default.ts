7ac860da2bc065a3e64cfe8eb5ffc51d7a8fef96
Afterglow79 <cakeby11@gmail.com>
2026-05-22 21:14:39 -0400

    begin update of battle scenes

--- meet-the-ottomans/src/world/scenes/default.ts
+++ meet-the-ottomans/src/world/scenes/default.ts
@@ -189,7 +189,7 @@ unloadAll(app);
                    new Battle(2, [50.4637, 2.1389], "Battle of Agincourt", new Entity()),
                    new Battle(2, [47.9025, 1.9090], "Siege of Orléans", new Entity()),
                    new Battle(2, [41.0151, 28.9793], "Fall of Constantinople", new Entity()),
-                   new Battle(3, [38.25, 21.25], "Battle of Lepanto", new Entity()),
+                   new Battle(3, [30.0667, 31.2167], "Battle of Ridaniya", new Entity()),
                    new Battle(3, [45.183, 9.150], "Battle of Pavia (Italian Wars)", new Entity()),
                    new Battle(3, [48.2017, 16.3350], "Siege of Vienna", new Entity()), // winged hussars my goat
                    new Battle(4, [37.2388, -76.5098], "Battle of Yorktown", new Entity()),
