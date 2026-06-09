ea1a496a91d109e0fd3c0b1160d3faf8b33c7202
Daniel Miranda <danielmcollegemc@gmail.com>
2026-06-09 21:48:36 +0000

    men can move freely with no master, FREE THE Slaves

--- meet-the-ottomans/src/world/npc/troops/frenchSoldier.ts
+++ meet-the-ottomans/src/world/npc/troops/frenchSoldier.ts
@@ -22,7 +22,7 @@ export class FrenchSoldier extends npc {
         super(id, "foe", 100, modelEntity);
         this.aiConfig.chaseMoveSpeed = PLAYER_MOVE_SPEED * 0.85;
         this.aiConfig.idleMoveSpeed = PLAYER_MOVE_SPEED * 0.7;
-        this.aiConfig.detectionRange = 34;
+        this.aiConfig.detectionRange = 200;
     }
 
     protected override getCombatProfile() {
